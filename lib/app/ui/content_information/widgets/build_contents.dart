f19b91aab7a4a321f8af931b24c2ca3b3653fd21
joaojsrbr <joaovitor.jsr@gmail.com>
2024-02-15 20:28:01 -0300

    extract CustomDismissible to ListDismissible

--- lib/app/ui/content_information/widgets/build_contents.dart
+++ lib/app/ui/content_information/widgets/build_contents.dart
@@ -1,12 +1,14 @@
 import 'package:app_wsrb_jsr/app/models/anime.dart';
 import 'package:app_wsrb_jsr/app/models/book.dart';
 import 'package:app_wsrb_jsr/app/models/chapter.dart';
+import 'package:app_wsrb_jsr/app/models/content.dart';
+import 'package:app_wsrb_jsr/app/models/data_content.dart';
 import 'package:app_wsrb_jsr/app/models/episode.dart';
 import 'package:app_wsrb_jsr/app/ui/player/arguments/player_args.dart';
 import 'package:app_wsrb_jsr/app/ui/reading/arguments/reading_args.dart';
+import 'package:app_wsrb_jsr/app/ui/shared/widgets/list_dismissible.dart';
 import 'package:app_wsrb_jsr/app/utils/custom_log.dart';
 import 'package:app_wsrb_jsr/app/routes/routes.dart';
-import 'package:app_wsrb_jsr/app/ui/shared/widgets/dismissible.dart';
 import 'package:app_wsrb_jsr/app/ui/content_information/widgets/scope.dart';
 import 'package:app_wsrb_jsr/app/ui/shared/widgets/shimmer.dart';
 import 'package:flutter/material.dart';
@@ -34,61 +36,90 @@ class BuildContents extends StatelessWidget {
         BookInformationScope.dataContentsOf(context).reversed.elementAt(index);
     final content = BookInformationScope.contentOf(context);
 
-    return SliverList.builder(
-      itemBuilder: (context, index) {
-        final dataContent = dataContents[index];
-
-        return CustomDismissible(
-          onUpdate: (details) {},
-          dismissThresholds: const {
-            DismissDirection.endToStart: 0.5,
-            DismissDirection.startToEnd: 0.5
-          },
-          resizeDuration: const Duration(milliseconds: 600),
-          background: Container(
-            alignment: Alignment.centerLeft,
-            decoration: const BoxDecoration(color: Colors.blueAccent),
-            padding: const EdgeInsets.only(left: 20.0),
-            child: const Icon(Icons.check, color: Colors.white),
-          ),
-          radius: 20,
-          secondaryBackground: Container(
-            decoration: const BoxDecoration(color: Colors.redAccent),
-            alignment: Alignment.centerRight,
-            padding: const EdgeInsets.only(right: 20.0),
-            child: const Icon(Icons.delete, color: Colors.white),
-          ),
-          key: ValueKey(dataContent.id),
-          onTap: () async {
-            customLog(
-              'tapped name: ${dataContent.title} - id: ${dataContent.id}',
-            );
-
-            if (dataContent is Chapter && content is Book) {
-              await context.push(
-                RouteName.READ,
-                extra: ReadingViewArgs(
-                  bookThemeData: Theme.of(context),
-                  chapter: dataContent,
-                  allDataContent: dataContents,
-                  currentIndex: index,
-                  book: content,
-                ),
-              );
-            } else if (dataContent is Episode && content is Anime) {
-              await context.push(
-                RouteName.PLAYER,
-                extra: PlayerArgs(anime: content, episode: dataContent),
-              );
-            }
-          },
-          child: ListTile(
-            titleTextStyle: Theme.of(context).textTheme.labelLarge,
-            title: Text(dataContent.title),
-          ),
+    return ListDismissible(
+      isSliver: true,
+      titleTextStyle: Theme.of(context).textTheme.labelLarge,
+      contents: DataContents.fromList(dataContents),
+      onTap: (DataContent data) async {
+        customLog(
+          'tapped name: ${data.title} - id: ${data.id}',
         );
+
+        if (data is Chapter && content is Book) {
+          await context.push(
+            RouteName.READ,
+            extra: ReadingViewArgs(
+              bookThemeData: Theme.of(context),
+              chapter: data,
+              allDataContent: dataContents,
+              currentIndex: index,
+              book: content,
+            ),
+          );
+        } else if (data is Episode && content is Anime) {
+          await context.push(
+            RouteName.PLAYER,
+            extra: PlayerArgs(anime: content, episode: data),
+          );
+        }
       },
-      itemCount: dataContents.length,
     );
+
+    // return SliverList.builder(
+    //   itemBuilder: (context, index) {
+    //     final dataContent = dataContents[index];
+
+    //     return CustomDismissible(
+    //       onUpdate: (details) {},
+    //       dismissThresholds: const {
+    //         DismissDirection.endToStart: 0.5,
+    //         DismissDirection.startToEnd: 0.5
+    //       },
+    //       resizeDuration: const Duration(milliseconds: 600),
+    //       background: Container(
+    //         alignment: Alignment.centerLeft,
+    //         decoration: const BoxDecoration(color: Colors.blueAccent),
+    //         padding: const EdgeInsets.only(left: 20.0),
+    //         child: const Icon(Icons.check, color: Colors.white),
+    //       ),
+    //       radius: 20,
+    //       secondaryBackground: Container(
+    //         decoration: const BoxDecoration(color: Colors.redAccent),
+    //         alignment: Alignment.centerRight,
+    //         padding: const EdgeInsets.only(right: 20.0),
+    //         child: const Icon(Icons.delete, color: Colors.white),
+    //       ),
+    //       key: ValueKey(dataContent.id),
+    //       onTap: () async {
+    //         customLog(
+    //           'tapped name: ${dataContent.title} - id: ${dataContent.id}',
+    //         );
+
+    //         if (dataContent is Chapter && content is Book) {
+    //           await context.push(
+    //             RouteName.READ,
+    //             extra: ReadingViewArgs(
+    //               bookThemeData: Theme.of(context),
+    //               chapter: dataContent,
+    //               allDataContent: dataContents,
+    //               currentIndex: index,
+    //               book: content,
+    //             ),
+    //           );
+    //         } else if (dataContent is Episode && content is Anime) {
+    //           await context.push(
+    //             RouteName.PLAYER,
+    //             extra: PlayerArgs(anime: content, episode: dataContent),
+    //           );
+    //         }
+    //       },
+    //       child: ListTile(
+    //         titleTextStyle: Theme.of(context).textTheme.labelLarge,
+    //         title: Text(dataContent.title),
+    //       ),
+    //     );
+    //   },
+    //   itemCount: dataContents.length,
+    // );
   }
 }
---
+++ lib/app/ui/shared/widgets/list_dismissible.dart
@@ -0,0 +1,81 @@
+import 'package:app_wsrb_jsr/app/core/extensions/custom_extensions/state_extensions.dart';
+import 'package:app_wsrb_jsr/app/models/content.dart';
+import 'package:app_wsrb_jsr/app/models/data_content.dart';
+import 'package:app_wsrb_jsr/app/ui/shared/widgets/dismissible.dart'
+    as customdismissible;
+import 'package:flutter/material.dart';
+
+class ListDismissible<T extends DataContent> extends StatelessWidget {
+  const ListDismissible({
+    super.key,
+    required this.contents,
+    this.selected,
+    this.physics,
+    this.onUpdate,
+    this.padding,
+    this.isSliver = false,
+    this.titleTextStyle,
+    this.resizeDuration = const Duration(milliseconds: 700),
+    this.onTap,
+  });
+  final TextStyle? titleTextStyle;
+  final DataContents contents;
+  final bool Function(DataContent content)? selected;
+  final ScrollPhysics? physics;
+  final bool isSliver;
+  final Duration resizeDuration;
+  final SetCallBack<T>? onTap;
+  final EdgeInsetsGeometry? padding;
+  final SetCallBack<customdismissible.DismissUpdateDetails>? onUpdate;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget itemBuilder(BuildContext context, int index) {
+      final content = contents[index];
+      return customdismissible.CustomDismissible(
+        onUpdate: onUpdate,
+        dismissThresholds: const {
+          DismissDirection.endToStart: 0.5,
+          DismissDirection.startToEnd: 0.5
+        },
+        resizeDuration: resizeDuration,
+        background: Container(
+          alignment: Alignment.centerLeft,
+          decoration: const BoxDecoration(color: Colors.blueAccent),
+          padding: const EdgeInsets.only(left: 20.0),
+          child: const Icon(Icons.check, color: Colors.white),
+        ),
+        radius: 20,
+        secondaryBackground: Container(
+          decoration: const BoxDecoration(color: Colors.redAccent),
+          alignment: Alignment.centerRight,
+          padding: const EdgeInsets.only(right: 20.0),
+          child: const Icon(Icons.delete, color: Colors.white),
+        ),
+        key: ValueKey(content.id),
+        onTap: (selected?.call(content) ?? true)
+            ? null
+            : () => onTap?.call(content as T),
+        child: ListTile(
+          selected: selected?.call(content) ?? false,
+          titleTextStyle: titleTextStyle,
+          title: Text(content.title),
+        ),
+      );
+    }
+
+    if (isSliver) {
+      return SliverList.builder(
+        itemBuilder: itemBuilder,
+        itemCount: contents.length,
+      );
+    }
+
+    return ListView.builder(
+      physics: physics,
+      padding: padding,
+      itemCount: contents.length,
+      itemBuilder: itemBuilder,
+    );
+  }
+}
