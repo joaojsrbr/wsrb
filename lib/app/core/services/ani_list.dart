4cdfeb42a7557f67d3a30c041d346abf3ca2b187
joaojsrbr <joaovitor.jsr@gmail.com>
2024-02-22 14:10:22 -0300

    commented class

--- lib/app/core/services/ani_list.dart
+++ lib/app/core/services/ani_list.dart
@@ -1,45 +1,45 @@
-import 'package:app_wsrb_jsr/app/core/constants/app.dart';
-import 'package:app_wsrb_jsr/app/repositories/ani_list_repository.dart';
-import 'package:graphql_flutter/graphql_flutter.dart';
+// import 'package:app_wsrb_jsr/app/core/constants/app.dart';
+// import 'package:app_wsrb_jsr/app/repositories/ani_list_repository.dart';
+// import 'package:graphql_flutter/graphql_flutter.dart';
 
-class AniListService implements AniListRepository {
-  late final AniListRepository _aniListRepository;
+// class AniListService implements AniListRepository {
+//   late final AniListRepository _aniListRepository;
 
-  late final GraphQLClient _client;
+//   late final GraphQLClient _client;
 
-  AniListService() {
-    final HttpLink httpLink = HttpLink(App.ANI_LIST);
+//   AniListService() {
+//     final HttpLink httpLink = HttpLink(App.ANI_LIST);
 
-    _client = GraphQLClient(cache: GraphQLCache(), link: httpLink);
+//     _client = GraphQLClient(cache: GraphQLCache(), link: httpLink);
 
-    _aniListRepository = AniListRepositoryImpl(_client);
-  }
+//     _aniListRepository = AniListRepositoryImpl(_client);
+//   }
 
-  static const String queryCoverImage = r"""
-    query ($search: String){
-          Media (search: $search) {
-              id
-              title {
-                  romaji
-              }
-              bannerImage
-              coverImage {
-                  medium
-                  large
-                  extraLarge
-              }
-          }
-      }
-    """;
+//   static const String queryCoverImage = r"""
+//     query ($search: String){
+//           Media (search: $search) {
+//               id
+//               title {
+//                   romaji
+//               }
+//               bannerImage
+//               coverImage {
+//                   medium
+//                   large
+//                   extraLarge
+//               }
+//           }
+//       }
+//     """;
 
-  @override
-  Future<QueryResult> post({
-    required String query,
-    Map<String, dynamic> variables = const {},
-  }) async {
-    return await _aniListRepository.post(
-      query: query,
-      variables: variables,
-    );
-  }
-}
+//   @override
+//   Future<QueryResult> post({
+//     required String query,
+//     Map<String, dynamic> variables = const {},
+//   }) async {
+//     return await _aniListRepository.post(
+//       query: query,
+//       variables: variables,
+//     );
+//   }
+// }
--- lib/app/repositories/ani_list_repository.dart
+++ lib/app/repositories/ani_list_repository.dart
@@ -1,29 +1,29 @@
-import 'package:graphql_flutter/graphql_flutter.dart';
+// import 'package:graphql_flutter/graphql_flutter.dart';
 
-abstract interface class AniListRepository {
-  Future<QueryResult> post({
-    required String query,
-    Map<String, dynamic> variables = const {},
-  });
-}
+// abstract interface class AniListRepository {
+//   Future<QueryResult> post({
+//     required String query,
+//     Map<String, dynamic> variables = const {},
+//   });
+// }
 
-class AniListRepositoryImpl implements AniListRepository {
-  final GraphQLClient _client;
+// class AniListRepositoryImpl implements AniListRepository {
+//   final GraphQLClient _client;
 
-  const AniListRepositoryImpl(this._client);
+//   const AniListRepositoryImpl(this._client);
 
-  @override
-  Future<QueryResult> post({
-    required String query,
-    Map<String, dynamic> variables = const {},
-  }) async {
-    final QueryOptions options = QueryOptions(
-      document: gql(query),
-      variables: variables,
-    );
+//   @override
+//   Future<QueryResult> post({
+//     required String query,
+//     Map<String, dynamic> variables = const {},
+//   }) async {
+//     final QueryOptions options = QueryOptions(
+//       document: gql(query),
+//       variables: variables,
+//     );
 
-    final QueryResult result = await _client.query(options);
+//     final QueryResult result = await _client.query(options);
 
-    return result;
-  }
-}
+//     return result;
+//   }
+// }
