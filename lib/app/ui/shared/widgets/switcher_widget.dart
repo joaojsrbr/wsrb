d12d7518d631b3158a7cdb4951ecc94b76fd32d8
João Vitor <joaovitor.jsr@gmail.com>
2025-04-01 14:45:32 -0300

    feat: adiciona SwitcherWidget para gerenciamento de animações e troca de widgets

---
+++ lib/app/ui/shared/widgets/switcher_widget.dart
@@ -0,0 +1,33 @@
+import 'package:content_library/content_library.dart';
+import 'package:flutter/material.dart';
+
+class SwitcherWidget extends StatelessWidget {
+  const SwitcherWidget({
+    super.key,
+    this.children = const [
+      SizedBox.shrink(),
+    ],
+    this.duration,
+    this.index = 1,
+  });
+
+  final List<Widget> children;
+  final Duration? duration;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    final index = this.index - 1;
+    final children = this.children.mapIndexed(
+          (index, e) => KeyedSubtree(
+            key: ObjectKey("${e.runtimeType}_$index"),
+            child: e,
+          ),
+        );
+    final widget = children.elementAt(index);
+
+    return duration != null
+        ? AnimatedSwitcher(duration: duration!, child: widget)
+        : widget;
+  }
+}
